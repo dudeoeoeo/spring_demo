86d07b25a0652064710378f2454b3e117fbdadaf
khy <hyunyoung0103@naver.com>
2021-09-01 15:13:36 +0900

    docker_env_finish_0.0.1

--- backend_spring/src/main/java/com/docker_exam/backend_spring/BackendSpringApplication.java
+++ backend_spring/src/main/java/com/docker_exam/backend_spring/BackendSpringApplication.java
@@ -2,6 +2,7 @@ package com.docker_exam.backend_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +14,12 @@ public class BackendSpringApplication {
 	}
 
 	@GetMapping("/")
-	public String hel() {
-		return "hello";
+	public String name() {
+		return "hello world";
 	}
 
+	@GetMapping("/hel")
+	public String doc() {
+		return "docker hello";
+	}
 }
